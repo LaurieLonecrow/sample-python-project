f2dd942451d409d8c8b56af4352a6bbff158cc82
Laurie Lay <llay@ippon.fr>
2024-02-02 16:14:42 -0500

    add simple api to call

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='simple_api',
+      version='1.0',
+      description='A Simple API',
+      author='Laurie Lay',
+      author_email='llay@ippon.fr',
+      packages=find_packages(),
+      )
---
+++ src/simple_api/__main__.py
@@ -0,0 +1,9 @@
+import uvicorn
+from simple_api.api import app
+
+def main():
+  uvicorn.run(app)
+
+  if __name__ == "__main__":
+    main()
+    
